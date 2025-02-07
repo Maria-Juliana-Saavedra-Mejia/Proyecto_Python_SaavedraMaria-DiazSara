5c51e83225202e9e1fc5a33721dbb96ef68d6eb7
Maria-Juliana-Saavedra-Mejia <saavedra.mariaju@gmail.com>
2025-02-07 12:42:41 -0500

    primera entrega

---
+++ modulocamper.py
@@ -0,0 +1,11 @@
+import json
+def abrirJSON():
+    dicFinal={}
+    with open('./camper.json','r') as openFile:
+        dicFinal=json.load(openFile)
+    return dicFinal
+
+def guardarJSON(dic):
+    with open("./camper.json",'w') as outFile:
+        json.dump(dic,outFile)
+
